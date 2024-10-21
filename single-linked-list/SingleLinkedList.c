cb41ccbfd80d84bc6f0696583636254fdef0a120
Philip Baumann <philip.baumann@hispeed.ch>
2024-10-21 21:52:33 +0200

    Refactor SingleLinkedList.c

--- single-linked-list/SingleLinkedList.c
+++ single-linked-list/SingleLinkedList.c
@@ -2,38 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 struct Node {
-	char value[30];
+	int value;
 	struct Node* next;
 };
 
-void printList(struct Node* node);
+struct SingleLinkedList {
+    struct Node* header;
+    struct Node* tailer;
+    int size;
+};
+
+void addFirst(struct SingleLinkedList* list, int value);
+int removeFirst(struct SingleLinkedList* list);
+void print(struct SingleLinkedList* list);
+struct SingleLinkedList* createSingleLinkedList();
 
 int main(int argc, char *argv[]) {
-	const int lengthOfArray = atoi(argv[1]);
-    struct Node* head = NULL;
 
-	printf("Length of list to be generated: %d\n", lengthOfArray);	
+    struct SingleLinkedList* list = createSingleLinkedList();
 
-    for (int i = 0; i < lengthOfArray; i++) {
-        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-        newNode->next = head;
-        strcpy(newNode->value, "Hello World");
-        head = newNode;
-    }
+    addFirst(list, 3);
+    addFirst(list, 2);
+    addFirst(list, 1);
+    addFirst(list, 3);
 
-    printList(head); 
-	
-	return 0; 
+    removeFirst(list);
+
+    print(list);
+
 }
 
-void printList(struct Node* node) {
-   struct Node* temp = node;
-    
-    while(temp != NULL) {
-      printf("%s \n", node->value);
-      temp = temp->next;
+struct SingleLinkedList* createSingleLinkedList() {
+    struct Node* header = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* tailer = (struct Node*)malloc(sizeof(struct Node));
+    struct SingleLinkedList* list = (struct SingleLinkedList*)malloc(sizeof(struct SingleLinkedList));
+    list->header = header;
+    list->tailer = tailer;
+    header->next = tailer;
+    return list;
+}
+
+void addFirst(struct SingleLinkedList* list, int value){
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* first = list->header->next;
+    node->value = value;
+    list->header->next = node;
+    node->next = first;
+    list->header->next = node;
+    node->next = first; 
+    list->size++;
+}
+
+int removeFirst(struct SingleLinkedList* list) {
+    if (list->size == 0){
+        return -1;
     }
-    printf("Finished");
+    
+    struct Node* first = list->header->next;
+    int value = first->value;
+    list->header->next = first->next;
+    return value; 
+}
+
+void print(struct SingleLinkedList* list) {
+    struct Node* temp = list->header->next;
+    while(temp != list->tailer){
+        printf("Value: %d\n", temp->value); 
+        temp = temp->next;
+    } 
 }
